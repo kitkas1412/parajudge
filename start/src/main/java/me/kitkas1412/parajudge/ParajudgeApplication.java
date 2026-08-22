9c5eeba6e150f6cb07c002d02af78fd0dcc8d7b3
dinhduc <ducndhe171600@fpt.edu.vn>
2026-08-22 11:05:32 +0700

    chore: initial commit
    Set up project structure and base configuration

---
+++ start/src/main/java/me/kitkas1412/parajudge/ParajudgeApplication.java
@@ -0,0 +1,13 @@
+package me.kitkas1412.parajudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParajudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParajudgeApplication.class, args);
+    }
+
+}
